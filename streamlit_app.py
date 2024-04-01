50c3f6ebfe08758513d1d41a692de7bf7056779e
Abhijit Das <abhijit1991@hotmail.com>
2024-04-01 14:00:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if time_to_insert:
     if ingredients_list: 
         ingredients_string = " "
         for fruit_chosen in ingredients_list:
-            ingredient_string += fruit_chosen + ''
+            ingredients_string += fruit_chosen + ''
             fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
             fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
     
