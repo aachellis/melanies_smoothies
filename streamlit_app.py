1110cc61fcdbca488e9383488676de8836dadafd
Abhijit Das <abhijit1991@hotmail.com>
2024-04-01 13:52:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if time_to_insert:
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
